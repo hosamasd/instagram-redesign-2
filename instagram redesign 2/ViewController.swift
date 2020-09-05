a6302d55ba6f82ddf9e1cfe33f6a96be4eb42807
hosam mohamed <hosamelmalt@gmail.com>
2020-09-05 14:13:03 -0700

    Initial Commit

---
+++ instagram redesign 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagram redesign 2
+//
+//  Created by Hossam on 9/5/20.
+//  Copyright © 2020 Hossam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
